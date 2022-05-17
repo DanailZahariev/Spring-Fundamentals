1f47582f5a7136ff3272fb03cb58ce6b4167150a
Danail <d.zahariev@icloud.com>
2022-05-17 11:54:20 +0300

    Initial commit

---
+++ SpringMobilele/src/test/java/com/example/SpringMobilele/SpringMobileleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SpringMobilele;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMobileleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

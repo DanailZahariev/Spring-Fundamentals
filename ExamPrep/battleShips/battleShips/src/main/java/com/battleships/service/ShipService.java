e5da36296a744433b091b20e31c8902adea40471
Danail <d.zahariev@icloud.com>
2022-06-17 00:22:13 +0300

    Update

--- ExamPrep/battleShips/battleShips/src/main/java/com/battleships/service/ShipService.java
+++ ExamPrep/battleShips/battleShips/src/main/java/com/battleships/service/ShipService.java
@@ -10,6 +10,7 @@ import com.battleships.repository.ShipRepository;
 import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
 
+import java.util.Comparator;
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
@@ -34,26 +35,16 @@ public class ShipService {
     }
 
     public List<ShipViewModel> findAll() {
-
         return shipRepository.findAll().stream().map(s -> modelMapper.map(s, ShipViewModel.class))
-                .sorted((s1, s2) -> {
-                    if (s1.getName().compareTo(s2.getName()) == 0) {
-
-                        if (s1.getHealth().compareTo(s2.getHealth()) == 0) {
-                            return s1.getPower().compareTo(s2.getPower());
-                        } else {
-                            return s1.getHealth().compareTo(s2.getHealth());
-                        }
-                    } else {
-                        return s1.getName().compareTo(s2.getName());
-                    }
-                }).collect(Collectors.toList());
+                .sorted(Comparator.comparing(ShipViewModel::getName))
+                .sorted(Comparator.comparing(ShipViewModel::getHealth))
+                .sorted(Comparator.comparing(ShipViewModel::getPower))
+                .collect(Collectors.toList());
     }
 
     public void fight(HomeBindingModel homeBindingModel) {
         Optional<Ship> attacker = shipRepository.findById(homeBindingModel.getAttackerShipId());
         Optional<Ship> defender = shipRepository.findById(homeBindingModel.getDefenderShipId());
-
 
         if (attacker.isEmpty() || defender.isEmpty()) {
             return;
